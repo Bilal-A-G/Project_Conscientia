413b4abc1186bd61d2543c64f822b0b010190d88
Bilal-A-G <86686062+Bilal-A-G@users.noreply.github.com>
2023-10-12 10:47:32 -0400

    Restarting from scratch, basically

--- Assets/Scripts/SpiderBrain.cs
+++ Assets/Scripts/SpiderBrain.cs
@@ -5,41 +5,17 @@ using UnityEngine;
 public class SpiderBrain : MonoBehaviour
 {
     [SerializeField] private Transform target;
-    [SerializeField] private Transform runnerTarget;
     [SerializeField] private Transform spider;
     [SerializeField] private float movementSpeed;
-    [SerializeField] private float rotationSpeed;
-    [SerializeField] private float stopRotatingTreshold;
-
-    [SerializeField] private float wallClimbDistance;
-    [SerializeField] private float heightOffGround;
-
-    private Transform currentWall;
-    private Vector3 newUpVec;
+    [SerializeField] private float stopChaseThreshold;
 
     void Update()
     {
-        spider.position = Vector3.Lerp(spider.position, target.position + spider.up * heightOffGround, Time.deltaTime * movementSpeed);
+        Vector3 toTarget = target.position - spider.position;
 
-        if ((runnerTarget.position - spider.position).magnitude <= stopRotatingTreshold)
+        if (toTarget.magnitude <= stopChaseThreshold)
             return;
 
-        RaycastHit wall;
-        if(Physics.Raycast(spider.position, spider.forward, out wall))
-        {
-            if((spider.position - wall.point).magnitude <= wallClimbDistance && 
-                wall.transform.gameObject.name == "Wall" &&
-                (currentWall == null || wall.transform.gameObject != currentWall.gameObject))
-            {
-                currentWall = wall.transform;
-                newUpVec = (spider.position - wall.point).normalized;
-            }
-        }
-
-        if (currentWall == null)
-            return;
-
-        spider.up = Vector3.Lerp(spider.up, newUpVec, Time.deltaTime * rotationSpeed);
-
+        spider.position += toTarget.normalized * Time.deltaTime * movementSpeed;
     }
 }
--- Assets/Scripts/SpiderLegController.cs
+++ Assets/Scripts/SpiderLegController.cs
@@ -10,105 +10,81 @@ namespace PROJECT_CONSCIENTIA
         [SerializeField] private Transform legTarget;
         [SerializeField] private Transform spiderBody;
 
-        [SerializeField] private float forwardsStepTreshold;
-        [SerializeField] private float backwardsStepTreshold;
-        [SerializeField] private float minLegSpacingFromBody;
+        [SerializeField] private float stepTreshold;
         [SerializeField] private float stepHeight;
         [SerializeField] private float stepSpeed;
 
-        [SerializeField] private float legExtension;
-        [Range(0,1)][SerializeField] private float legSplay;
-
-        [SerializeField] private float legDelay;
         [SerializeField] private bool rightLeg;
         [SerializeField] private bool frontLeg;
+        [SerializeField] private float velocityPrediction;
+
+        [SerializeField] private List<SpiderLegController> oppositeLegs;
 
         [Header("Debug")]
         [SerializeField] private float sphereRadius;
 
         private float lerp;
 
-        private Vector3 toNewTarget;
+        private Vector3 toNextPosition;
         private Vector3 bezierTopPoint;
-        private RaycastHit nextStepHit;
+        private Vector3 nextPosition;
 
-        bool legMoving;
-        bool evaluatingLegPos;
+        private bool legMoving;
+        private Vector3 spiderPosLastFrame;
+
+        private void Start()
+        {
+            spiderPosLastFrame = spiderBody.position;
+        }
 
         void Update()
         {
             lerp += Time.deltaTime * stepSpeed;
 
-            if (!evaluatingLegPos)
-            {
-                StartCoroutine(MoveLeg());
-            }
+            MoveLeg();
 
             if (!legMoving)
                 return;
 
-            legTarget.position = Vector3.Lerp(
+            legTarget.position = 
+                Vector3.Lerp(
             Vector3.Lerp(legTarget.position, bezierTopPoint, lerp),
-            Vector3.Lerp(bezierTopPoint, nextStepHit.point, lerp),
+            Vector3.Lerp(bezierTopPoint, nextPosition, lerp),
             lerp);
 
-            if (legTarget.position == nextStepHit.point)
+            if (legTarget.position == nextPosition)
             {
                 legMoving = false;
             }
+
+            spiderPosLastFrame = spiderBody.position;
         }
 
-        IEnumerator MoveLeg()
+        private void MoveLeg()
         {
-            evaluatingLegPos = true;
+            RaycastHit stepHit;
 
-            yield return new WaitForSeconds(legDelay);
-
-            legRaycastOrigin.position = spiderBody.position + Vector3.Lerp(
-                (frontLeg ? spiderBody.forward : -spiderBody.forward) * legExtension,
-                (rightLeg ? spiderBody.right : -spiderBody.right) * legExtension, legSplay) ;
-
-            if (Physics.Raycast(legRaycastOrigin.position, -spiderBody.up, out nextStepHit))
+            if (Physics.Raycast(legRaycastOrigin.position, -spiderBody.up, out stepHit))
             {
-                RaycastHit wallHit;
-                Vector3 toTarget = (nextStepHit.point - spiderBody.position).normalized;
-
-                if (Physics.Raycast(spiderBody.position, toTarget, out wallHit))
-                {
-                    if (wallHit.transform.gameObject != nextStepHit.transform.gameObject)
-                    {
-                        nextStepHit = wallHit;
-                    }
-                }
-
-                float stepTreshold = forwardsStepTreshold;
-
-                if (Vector3.Dot((nextStepHit.point - legTarget.position).normalized, -spiderBody.forward) >= 0.8f)
-                {
-                    stepTreshold = backwardsStepTreshold;
-                }
-
-                if ((nextStepHit.point - legTarget.position).magnitude >= stepTreshold &&
-                    (spiderBody.position - nextStepHit.point).magnitude >= minLegSpacingFromBody &&
+                if ((stepHit.point - legTarget.position).magnitude >= stepTreshold && 
                     !legMoving)
                 {
-                    toNewTarget = nextStepHit.point - legTarget.position;
+                    nextPosition = (stepHit.point + (spiderBody.position - spiderPosLastFrame).normalized * velocityPrediction);
+                    toNextPosition = nextPosition - legTarget.position;
                     bezierTopPoint = legTarget.position +
-                        toNewTarget.normalized * toNewTarget.magnitude / 2 +
+                        toNextPosition.normalized * toNextPosition.magnitude / 2 +
                         spiderBody.up * stepHeight;
 
                     lerp = 0;
                     legMoving = true;
                 }
             }
-
-            evaluatingLegPos = false;
         }
 
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.red;
-            Gizmos.DrawSphere(nextStepHit.point, sphereRadius);
+            Gizmos.DrawSphere(nextPosition, sphereRadius);
         }
     }
 }
