e7b42735485f9ddf7c2993bbc20b235433d02a05
Bilal-A-G <86686062+Bilal-A-G@users.noreply.github.com>
2023-10-14 17:52:15 -0400

    Solved spider ground movement, now need to implement looking and wall climbing

--- Assets/Scripts/MonsterIKController.cs
+++ Assets/Scripts/MonsterIKController.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class MonsterIKController : MonoBehaviour
 {
+    [Header("Inverse Kinematics")]
     [SerializeField] private Leg[] legs;
     [SerializeField] private Transform spider;
-
     [SerializeField] private float stepTreshold;
     [SerializeField] private float stepHeight;
     [SerializeField] private float stepSpeed;
+
+    [Header("Movement")]
+    [SerializeField] private Transform runner;
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float stopChaseDistance;
+    [SerializeField] private float yawSpeed;
+    [SerializeField] private float pitchSpeed;
+    [SerializeField] private float velocityPrediction;
 
     private Leg currentMovingLeg = null;
     private int currentMovingLegIndex;
@@ -17,14 +25,23 @@ public class MonsterIKController : MonoBehaviour
     private float lerp;
 
     private Dictionary<int, float> legsToMoveIndicesAndDistances;
+    private Vector3 newForward;
+    private Vector3 newUp;
+    private Vector3 spiderPosLastFrame;
 
     private void Start()
     {
         legsToMoveIndicesAndDistances = new Dictionary<int, float>();
+
+        newUp = spider.up;
+        newForward = spider.forward;
+        spiderPosLastFrame = spider.position;
     }
 
     void Update()
     {
+        MoveAndRotate();
+
         for (int i = 0; i < legs.Length; i++)
         {
             RaycastHit hit;
@@ -40,7 +57,7 @@ public class MonsterIKController : MonoBehaviour
 
                 if ((hit.point - currentLeg.Iktarget.position).magnitude > stepTreshold)
                 {
-                    currentLeg.nextPosition = hit.point;
+                    currentLeg.nextPosition = hit.point + (spider.position - spiderPosLastFrame).normalized * velocityPrediction;
                     currentLeg.previousPosition = currentPosition;
                     currentLeg.bezierTopPosition = currentPosition + (toTarget.normalized *
                         toTarget.magnitude / 2 + spider.up * stepHeight);
@@ -96,6 +113,39 @@ public class MonsterIKController : MonoBehaviour
         }
 
         legs[currentMovingLegIndex] = currentMovingLeg;
+
+        spiderPosLastFrame = spider.position;
+    }
+
+    private void MoveAndRotate()
+    {
+        Vector3 toRunner = runner.position - spider.position;
+
+        Vector3 backToFront =
+            ((legs[0].Iktarget.position + legs[2].Iktarget.position) / 2
+            -
+            (legs[3].Iktarget.position + legs[1].Iktarget.position) / 2).normalized;
+
+        Vector3 rightToLeft =
+            ((legs[0].Iktarget.position + legs[3].Iktarget.position) / 2
+            -
+            (legs[2].Iktarget.position + legs[1].Iktarget.position) / 2).normalized;
+
+        newUp = Vector3.Lerp(newUp, Vector3.Cross(rightToLeft, backToFront).normalized, Time.deltaTime);
+
+        newForward = Vector3.Lerp(newForward, toRunner.normalized, Time.deltaTime);
+
+        if (toRunner.magnitude > stopChaseDistance)
+        {
+            float rawAngleXZ = Mathf.Atan2(newForward.z, newForward.x) -
+                Mathf.Atan2(spider.forward.z, spider.forward.x);
+
+            float angleXZPlane = -Mathf.Rad2Deg * (Mathf.Abs(rawAngleXZ) > Mathf.PI ?
+                (rawAngleXZ > 0 ? rawAngleXZ + Mathf.PI : rawAngleXZ - Mathf.PI) : rawAngleXZ);
+
+            spider.position += movementSpeed * Time.deltaTime * toRunner.normalized;
+            spider.rotation *= Quaternion.Euler(new Vector3(0, angleXZPlane, 0).normalized * yawSpeed * Time.deltaTime).normalized;
+        }
     }
 }
 
